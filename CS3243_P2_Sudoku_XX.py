b2f75c25c2e660712cb579740efb20dbfe242d56
RusdiHaizim <rusdihaizim@gmail.com>
2020-05-05 16:12:46 +0800

    Big Pog here: 1) Experimental code added 2) New Heuristic added [HiddenSingles] 3) Cleaned up code with some doc

--- CS3243_P2_Sudoku_XX.py
+++ CS3243_P2_Sudoku_XX.py
@@ -3,13 +3,14 @@ from copy import deepcopy
 from Queue import Queue
 from time import time
 
-
 # Running script: given code can be run with the command:
 # python file.py, ./path/to/init_state.txt ./output/output.txt
 FAILURE = -1
 knownValues = {}
 fileIn = None
-### Class containing csp model i.e. variables, domains, edges btw variables, backupForReassignment
+
+
+# Class containing csp model i.e. variables, domains, edges btw variables, backupForReassignment
 class Csp(object):
     def __init__(self, puzzle):
         self.varList = list()
@@ -36,7 +37,7 @@ class Csp(object):
         for v in self.varList:
             self.domain[v] = set()
             if puzzle1[idx] == 0:
-                for j in range(1,10):
+                for j in range(1, 10):
                     self.domain[v].add(j)
             else:
                 self.domain[v].add(puzzle1[idx])
@@ -69,7 +70,7 @@ class Csp(object):
                         self.neighbours[var].add((i, j))
 
         # Init restore for each var, var:list()
-        self.restore = {v: list() for v in self.varList}
+        self.restore = {v: set() for v in self.varList}
 
     def isSolved(self):
         for var in self.varList:
@@ -92,18 +93,18 @@ class Csp(object):
         for nCell in self.neighbours[var]:
             if nCell not in assignment and value in self.domain[nCell]:
                 self.domain[nCell].remove(value)
-                self.restore[var].append((nCell, value))
+                self.restore[var].add((nCell, value))
 
     # DEPRECATED
     def unassign(self, var, assignment):
         if var in assignment:
             for (cell, value) in self.restore[var]:
                 self.domain[cell].append(value)
-            self.restore[var] = []
+            self.restore[var] = set()
             del assignment[var]
 
 
-### Class containing Sudoku solving methods: AC3 and Backtrack
+# Class containing Sudoku solving methods: AC3 and Backtrack
 class Sudoku(object):
     def __init__(self, puzzle):
         # you may add more attributes if you need
@@ -113,17 +114,20 @@ class Sudoku(object):
         self.constraints = self.getConstraints(self.csp)
         self.steps = 0
         self.timeTaken = 0
+        self.hiddenSingleFlag = True # Flag for finding HiddenSingles, set to True initially
 
     def copy(self, puzzle):
         new_puzzle = [[x for x in row] for row in puzzle]
         return new_puzzle
 
     def runAC3(self, csp, var=None, assignment=None):
-        qu = Queue()
-        for nCell in self.csp.neighbours[var]:
-            if nCell not in assignment:
+        if var is None or assignment is None:
+            qu = self.getConstraints(csp)
+        else:
+            qu = Queue()
+            for nCell in self.csp.neighbours[var]:
+                if nCell not in assignment:
                     qu.put((nCell, var))
-        # qu = self.getConstraints(csp)
         while True:
             if qu.empty():
                 break
@@ -132,7 +136,7 @@ class Sudoku(object):
                 if len(csp.domain[xI]) == 0:
                     return False
                 # Don't need propagate xI if it doesnt contain 1 value only
-                if len(csp.domain[xI]) != 1:
+                if var is not None and len(csp.domain[xI]) != 1:
                     continue
                 for xK in csp.neighbours[xI]:
                     if assignment is not None:
@@ -149,7 +153,7 @@ class Sudoku(object):
             if dI in csp.domain[xJ] and len(csp.domain[xJ]) == 1:
                 cop.remove(dI)
                 if var is not None:
-                    csp.restore[var].append((xI, dI))
+                    csp.restore[var].add((xI, dI))
                 revised = True
         csp.domain[xI] = cop
         return revised
@@ -173,55 +177,114 @@ class Sudoku(object):
         for val in self.csp.domain[var]:
             if val != value:
                 cop.remove(val)
-                self.csp.restore[var].append((var, val))
+                self.csp.restore[var].add((var, val))
         self.csp.domain[var] = cop
 
     def unassign(self, var, assignment):
         if var in assignment:
             for (cell, value) in self.csp.restore[var]:
                 self.csp.domain[cell].add(value)
-            self.csp.restore[var] = []
+            self.csp.restore[var] = set()
             del assignment[var]
+
+    def forwardCheck(self, assignment, var, value):
+        for nCell in self.csp.neighbours[var]:
+            if nCell not in assignment and value in self.csp.domain[nCell]:
+                self.csp.domain[nCell].remove(value)
+                self.csp.restore[var].add((nCell, value))
+        return True
 
     def runInference(self, assignment, var, value):
         return self.runAC3(self.csp, var, assignment)
-        # for nCell in self.csp.neighbours[var]:
-        #     if nCell not in assignment and value in self.csp.domain[nCell]:
-        #         self.csp.domain[nCell].remove(value)
-        #         self.csp.restore[var].append((nCell, value))
-        return True
+        # return self.forwardCheck(assignment, var, value)
 
     # Bulk of solve is here
     def backtrackSearch(self, assignment):
         if len(assignment) == len(self.csp.varList):
             return assignment
-        # Most constrained variable heuristic
+
+        # Select from 3 criteria (SEE function for more info)
         var = self.selectUnassignedVariable(assignment)
+        # Stop finding HiddenSingles if first occurrence of MRV has started
+        if not isinstance(var, list) and not len(self.csp.domain[var]) == 1 and self.hiddenSingleFlag:
+            self.hiddenSingleFlag = False
+
+        # Gives preference of value for HiddenSingle if found
+        prefVal = None
+        if isinstance(var, list):
+            prefVal = var[1]
+            var = var[0]
+
         # Least Constraining Value heuristic
-        for value in self.orderDomainValues(var):
-            # if self.csp.isConsistent(assignment, var, value):
+        for value in self.orderDomainValues(var, prefVal):
             if self.isConsistent(var, value):
                 self.assign(assignment, var, value)
-
                 # Do inference i.e. forward checking or ac-3 here
                 if self.runInference(assignment, var, value) != FAILURE:
                     result = self.backtrackSearch(assignment)
                     if result is not None:
                         return result
                     self.steps += 1
-
                 self.unassign(var, assignment)
-
         return None
 
-    # Function to return var with min number of domain values
+    # Function to decide which variable to select for backtracking
+    # 3 criteria: size-1-domain var, HiddenSingles, MRV
     def selectUnassignedVariable(self, assignment):
         unassigned = [v for v in self.csp.varList if v not in assignment]
+        minV = min(unassigned, key=lambda var: len(self.csp.domain[var]))
+        # Return early if minV is confirmed to have only 1 value
+        if len(self.csp.domain[minV]) == 1:
+            return minV
+
+        # Only find HiddenSingles if MRV isn't implemented yet [Idk why but it only works this way]
+        if self.hiddenSingleFlag:
+            for i in range(0, 9):
+                rowList = {v: [] for v in range(1, 10)}  # Checks for HiddenSingles by row
+                colList = {v: [] for v in range(1, 10)}  # Checks for HiddenSingles by col
+                for j in range(0, 9):
+                    for v in self.csp.domain[(i, j)]:
+                        if len(self.csp.domain[(i, j)]) != 1:
+                            rowList[v].append((i, j))
+                    for v in self.csp.domain[(j, i)]:
+                        if len(self.csp.domain[(j, i)]) != 1:
+                            colList[v].append((j, i))
+                for v in rowList:
+                    if len(rowList[v]) == 1:
+                        return [rowList[v][0], v]
+                for v in colList:
+                    if len(colList[v]) == 1:
+                        return [colList[v][0], v]
+
+            # Checks for HiddenSingles by box
+            bxLs = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]
+            for i in range(0, 9):
+                boxList = {v: [] for v in range(1, 10)}
+                rowCount = bxLs[i][0]
+                for y in range(rowCount, rowCount + 3):
+                    colCount = bxLs[i][1]
+                    for x in range(colCount, colCount + 3):
+                        if len(self.csp.domain[(y, x)]) == 1:
+                            continue
+                        for v in self.csp.domain[(y, x)]:
+                            boxList[v].append((y, x))
+                for v in boxList:
+                    if len(boxList[v]) == 1:
+                        return [boxList[v][0], v]
+
+        # MRV implemented if cannot find HiddenSingles
         return min(unassigned, key=lambda var: len(self.csp.domain[var]))
 
-    ##For now, return domain list of a variable
-    def orderDomainValues(self, var):
+    # Firstly, tries to return domain with preferential value for HiddenSingle at the front, if present
+    # Else, return normal domain of var
+    def orderDomainValues(self, var, prefVal=None):
         # Return value which gives least count of conflicts btw cells
+        if prefVal is not None:
+            prefList = []
+            for v in self.csp.domain[var]:
+                prefList.append(v)
+            prefList.insert(0, prefVal)
+            return prefList
         # return sorted(self.csp.domain[var], key=lambda val: self.getConflictingCount(var, val))
         return self.csp.domain[var]
 
@@ -241,12 +304,13 @@ class Sudoku(object):
         newPuzzle = self.csp
         pP(self.puzzle)
         startTime = time()
-        # timeTaken = 0
-        # print 'Starting backtrack'
 
         # RUN BACKTRACKING
         assignment = {}
         # assignment.update(knownValues)
+        # self.runAC3(newPuzzle)
+        # for key in sorted(newPuzzle.domain):
+        #     print key, newPuzzle.domain[key]
         assignment = self.backtrackSearch(assignment)
         if assignment is not None:
             print 'STEPS:', self.steps
@@ -263,7 +327,6 @@ class Sudoku(object):
             pP(self.ans)
             print 'Time:', self.timeTaken
             self.checkResult()
-
         else:
             # NO solution, returning original sudoku
             print 'Backtrack FAILED'
@@ -309,6 +372,7 @@ def pP(puzzle):
             print puzzle[i][j],
         print ""
 
+
 def returnPuzzle(f):
     lines = f.readlines()
     puzzle = [[0 for i in range(9)] for j in range(9)]
@@ -323,12 +387,18 @@ def returnPuzzle(f):
                     j = 0
     return puzzle
 
+
+TIME_UB = 3
+
+
+# Code for testing range of inputs
+# Will find hardest inputs that the algorithm is trying to solve
 def startTest(numRange):
     numRange = numRange.split(',')
     assert len(numRange) == 2
     fileStart = 'sudoku/batchtest/new_input'
 
-    #Start the experiment to find top 5 hardest puzzles
+    # Start the experiment to find top TIME_UB hardest puzzles
     hardest = []
     for i in range(int(numRange[0]), int(numRange[1]) + 1):
         try:
@@ -340,7 +410,7 @@ def startTest(numRange):
         puz = returnPuzzle(f)
         sud = Sudoku(puz)
         sud.solve()
-        if sud.timeTaken > 3:
+        if sud.timeTaken > TIME_UB:
             tup = (sud.timeTaken, puz, fileName.replace('sudoku/batchtest/', ''))
             hardest.append(tup)
 
@@ -350,7 +420,7 @@ def startTest(numRange):
         pP(hardest[i][1])
         print '@' * 15
 
-    # if len(sys.argv) == 2:
+    # if len(sys.argv) == 2: #Code for start of __main__, to run experiment
     #     print 'COMMENCING TEST'
     #     startTest(sys.argv[1])
     #     print 'END TEST'
